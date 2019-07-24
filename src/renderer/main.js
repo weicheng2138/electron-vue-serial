aea223c065a45d18b0ad9550ba466f4ad9feba40
ANHE-2 <ANHE-2@ANHE-2-PC>
2019-07-24 12:22:08 +0800

    setup everything

---
+++ src/renderer/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  template: '<App/>'
+}).$mount('#app')
+
+
