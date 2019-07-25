7dd03c8f0f95a888bb14ad6d0c0c22810cdb4fd2
ANHE-2 <ANHE-2@ANHE-2-PC>
2019-07-25 09:51:43 +0800

    dropdown test finished

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -34,29 +34,13 @@
               variant="primary"
               class="m-2"
             >
-              <!-- <b-dropdown-item 
+              <b-dropdown-item 
                 v-for="option in serialPorts" 
                 :key="option.comName" 
                 :value="option.comName"
                 @click="selectedPort = option.comName"
-              >{{ option.comName }}</b-dropdown-item> -->
-              <!-- <b-dropdown-item 
-                v-for="option in serialPorts" 
-                :key="option.comName" 
-                :value="option.comName"
-                @click="selectedPort = option.comName"
-              >{{ option.comName }}</b-dropdown-item> -->
+              >{{ option.comName }}</b-dropdown-item>
             </b-dropdown>
-
-            <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-              <b-dropdown-item>First Action</b-dropdown-item>
-              <b-dropdown-item>Second Action</b-dropdown-item>
-              <b-dropdown-item>Third Action</b-dropdown-item>
-              <b-dropdown-divider></b-dropdown-divider>
-              <b-dropdown-item active>Active action</b-dropdown-item>
-              <b-dropdown-item disabled>Disabled action</b-dropdown-item>
-            </b-dropdown>
-
           </div>
         </div>
       </div>
